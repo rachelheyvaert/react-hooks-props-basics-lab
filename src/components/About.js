69e128e99aa33d9cc212a7c5975af56a0d35a6a8
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-04-17 17:15:05 -0400

    complete assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-function About() {
+function About(props) {
+const isPassed = props.bio;
+  if(!isPassed) {
+    return null
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+    <a href={props.github} alt="github"></a>
+    <a href={props.linkedin} alt="linkedn"></a>
     </div>
   );
 }
